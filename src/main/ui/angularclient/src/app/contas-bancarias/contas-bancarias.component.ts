4bd933a4f08f90ad326015b8c8e9b63658d26315
Wendel <wendel.s.menegasso@gmail.com>
2023-07-23 22:17:32 -0300

    Lista de tipos de contas funcionando.

--- src/main/ui/angularclient/src/app/contas-bancarias/contas-bancarias.component.ts
+++ src/main/ui/angularclient/src/app/contas-bancarias/contas-bancarias.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContasBancariasService } from '../service/contas-bancarias.service';
 import { ContasBancarias } from '../model/contas-bancarias';
+import { TipoConta } from '../model/tipo-conta';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
@@ -13,7 +14,8 @@ import { Subject } from 'rxjs';
 export class ContasBancariasComponent implements OnInit {
 
   contas: ContasBancarias;
-  contasBancarias: any[] = [];
+  contasBancarias: ContasBancarias[] = [];
+  accounts: ContasBancarias[] = [];
   contasCount = 0;
     destroy$: Subject<boolean> = new Subject<boolean>();
 
@@ -28,10 +30,42 @@ export class ContasBancariasComponent implements OnInit {
     this.getTodasContas();
   }
 
-    getTodasContas() {
-      this.contasBancariasService.findAll().pipe(takeUntil(this.destroy$)).subscribe((contasBancarias: any[]) => {
+    getTodasContas(): void {
+      this.contasBancariasService.findAll().pipe(takeUntil(this.destroy$)).subscribe((contasBancarias: ContasBancarias[]) => {
   		    this.contasCount = contasBancarias.length;
-          this.contasBancarias = contasBancarias;
+  		    this.contasBancarias = contasBancarias;
+  		    var accounts :ContasBancarias[] = [];
+  		    for (var i=0; i<this.contasCount;i++){
+  		        var account: ContasBancarias = new ContasBancarias();
+  		        if (this.contasBancarias[i].tipo == "1"){
+  		            account.tipo = "Poupança";
+  		        }
+  		        if (this.contasBancarias[i].tipo == "2"){
+  		            account.tipo = "Salário";
+  		        }
+  		        if (this.contasBancarias[i].tipo == "3"){
+  		            account.tipo = "Corrente";
+  		        }
+  		        if (this.contasBancarias[i].tipo == "4"){
+  		            account.tipo = "Empresa";
+  		        }
+              account.id = this.contasBancarias[i].id;
+              account.agencia = this.contasBancarias[i].agencia;
+              account.banco = this.contasBancarias[i].banco;
+              account.saldo = this.contasBancarias[i].saldo;
+              account.conta = this.contasBancarias[i].conta;
+              account.usuario = this.contasBancarias[i].usuario;
+              accounts[i] = account;
+  		    }
+
+  		    for (var i=0; i<this.contasCount;i++){
+  		        this.contasBancarias.pop();
+  		    }
+  		    for (var i=0; i<this.contasCount;i++){
+  		        this.contasBancarias.push(accounts[i]);
+  		    }
+
+          console.log(this.contasBancarias);
       });
     }
     ngOnDestroy() {
