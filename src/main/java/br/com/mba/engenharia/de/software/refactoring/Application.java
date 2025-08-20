7f19d4183c831190e9db071c6aaeddb1c57eeae1
wende <wendel.s.menegasso@gmail.com>
2025-08-20 08:40:25 -0300

    Refatorando Application

--- src/main/java/br/com/mba/engenharia/de/software/refactoring/Application.java
+++ src/main/java/br/com/mba/engenharia/de/software/refactoring/Application.java
@@ -1,12 +1,12 @@
 package br.com.mba.engenharia.de.software.refactoring;
 
-import br.com.mba.engenharia.de.software.controller.*;
-import br.com.mba.engenharia.de.software.entity.contas.Conta;
-import br.com.mba.engenharia.de.software.entity.despesas.Gastos;
-import br.com.mba.engenharia.de.software.entity.imoveis.Imoveis;
-import br.com.mba.engenharia.de.software.entity.rendas.Renda;
-import br.com.mba.engenharia.de.software.entity.usuarios.Usuario;
-import br.com.mba.engenharia.de.software.entity.veiculos.Veiculos;
+import br.com.mba.engenharia.de.software.refactoring.controller.*;
+import br.com.mba.engenharia.de.software.refactoring.entity.contas.Conta;
+import br.com.mba.engenharia.de.software.refactoring.entity.despesas.Gastos;
+import br.com.mba.engenharia.de.software.refactoring.entity.imoveis.Imoveis;
+import br.com.mba.engenharia.de.software.refactoring.entity.rendas.Renda;
+import br.com.mba.engenharia.de.software.refactoring.entity.usuarios.Usuario;
+import br.com.mba.engenharia.de.software.refactoring.entity.veiculos.Veiculos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,9 +15,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Configuration
-@EnableJpaRepositories(basePackages = {"br.com.mba.engenharia.de.software.repository.contas", "br.com.mba.engenharia.de.software.repository.usuario",
-                                       "br.com.mba.engenharia.de.software.repository.rendas","br.com.mba.engenharia.de.software.repository.gastos",
-                                       "br.com.mba.engenharia.de.software.repository.veiculos","br.com.mba.engenharia.de.software.repository.imoveis"})
+@EnableJpaRepositories(basePackages = {"br.com.mba.engenharia.de.software.refactoring.repository.contas", "br.com.mba.engenharia.de.software.refactoring.repository.usuario",
+                                       "br.com.mba.engenharia.de.software.refactoring.repository.rendas","br.com.mba.engenharia.de.software.refactoring.repository.gastos",
+                                       "br.com.mba.engenharia.de.software.refactoring.repository.veiculos","br.com.mba.engenharia.de.software.refactoring.repository.imoveis"})
 @ComponentScan(basePackageClasses = {Renda.class, Conta.class, Usuario.class, Veiculos.class, Imoveis.class,
                                      LoginController.class, UsuarioController.class, ContaController.class, RendasController.class,
         GastosController.class, Gastos.class, OrigensController.class, DashboradController.class, VeiculosController.class,
